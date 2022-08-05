4dc9dc0a67a37191e5648d7272b6c9d6f18b3930
marksolyom <solyom.mark@gmail.com>
2022-08-05 14:11:04 +0200

    add main section and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <img src="./img/facebook-logo.png" alt="facebook logo" className="social-logo" />
+            </a>
+            <a href="https://github.com/" target="_blank" rel="noreferrer">
+            <img src="./img/github-logo.png" alt="github logo" className="social-logo" />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+            <img src="./img/instagram-logo.png" alt="instagram logo" className="social-logo" />
+            </a>
+            <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+            <img src="./img/linkedin-logo.png" alt="linkedin logo" className="social-logo" />
+            </a>
+            <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+            <img src="./img/twitter-logo.png" alt="twitter logo" className="social-logo" />
+            </a>
+        </footer>
+    )
+}
